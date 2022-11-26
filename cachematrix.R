f45eb4e0e83e3f1abe8939043e7629da9d88683d
JTRLearner <119095468+JTRLearner@users.noreply.github.com>
2022-11-26 02:34:46 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function yields a matrix that can be called on by a function that later yields the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+inverse <- NULL
+set <- function(y) {
+	x <<- y
+	inverse <<- NULL}
+get <- function() x
+setinverse <- function(INV) inverse <<- INV
+getinverse <- function(inverse)
 
+list(set = set, get=get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes the Cahce Matrix and determines if an inverse is calculable. 
+## If such a value is found and it differs from the original, the inverse is given. 
+## If not, the default value is given.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		inverse <- x$getinverse()
+		if(!is.null(inverse)) {
+			message("getting cached data!")
+			return(inverse) }
+	
+	data <- x$get()
+	inverse <- solve(data, ...)
+	x$setinverse(inverse)
+	inverse
 }
